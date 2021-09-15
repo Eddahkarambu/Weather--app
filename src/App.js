bb6bba8fae2e43c25092991f76082e7ad46ac745
Eddahkarambu <gikunaeddah@gmail.com>
2021-09-15 14:56:10 +0300

    updated app.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="Days"> 5 days Forecast
+      </div>
+
+      <div className="kenya">
+      <div className="country">Kenya
+      </div>
+
+      <div className="buttons">
+      <button >Celcius</button>
+      <button>Farenheit</button>
+      </div>
+      </div>
+
+      <div className="dow">
+        <div>Sunday</div>
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>
+        <div>Thursday</div>
+      </div>
+
+      <div className="dates">
+        <div>sunday, 1;00pm</div>
+        <div>Monday, 1;00pm</div>
+        <div>Tuesday, 1;00pm</div>
+        <div>Wednesday, 1;00pm</div>
+        <div>Thursday, 1;00pm</div>
+      </div>
     </div>
   );
 }
