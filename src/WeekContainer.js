b6a0f1cc5ec3699f14b2e7e31de44905dcc03901
Eddahkarambu <gikunaeddah@gmail.com>
2021-09-21 17:09:46 +0300

    saved changes

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -81,7 +81,7 @@ class WeekContainer extends React.Component{
                 <div className="container">
                     
                     <h1 className="display-1 jumbotron">5-Day Forecast.</h1>
-                    <h5 className="display-5 text-muted">Kenya</h5>
+                    <h5 className="display-5 text-muted">Kenya, Nairobi</h5>
                     <DegreeToggle degreeType={this.state.degreeType} updateForecastDegree={this.updateForecastDegree}/>
                     <h2>forecast</h2>
                     <div className="row justify-content-center">
